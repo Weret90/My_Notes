5213fcb1fa3e09c2415fbd12f46a46eb1b925195
Weret90 <apostol-ea@mail.ru>
2021-06-17 23:01:27 +0900

    Удалил интенты для реализации подхода Single Activity (заменил фрагментами). Добавил в тулбаре кнопку добавления заметки (пока что только макет, без реализации добавления). Создал боковое навигационное меню с инфой о приложении

--- app/src/main/java/com/umbrella/mynotes/MainActivity.java
+++ app/src/main/java/com/umbrella/mynotes/MainActivity.java
@@ -1,10 +1,19 @@
 package com.umbrella.mynotes;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NotesFragment.OnNoteClicked {
 
@@ -14,6 +23,32 @@ public class MainActivity extends AppCompatActivity implements NotesFragment.OnN
         setContentView(R.layout.activity_main);
 
         getSupportFragmentManager().beginTransaction().add(R.id.notes_titles, new NotesFragment()).commit();
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.navigation_menu);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+                getSupportFragmentManager().beginTransaction().replace(R.id.notes_titles, new InfoFragment()).addToBackStack(null).commit();
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.simple_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.notes_titles, new NoteAddFragment()).addToBackStack(null).commit();
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -22,12 +57,7 @@ public class MainActivity extends AppCompatActivity implements NotesFragment.OnN
         if (isLandscape) {
             getSupportFragmentManager().beginTransaction().replace(R.id.note_description_fragment, NotesDescriptionFragment.newInstance(note)).commit();
         } else {
-
             getSupportFragmentManager().beginTransaction().replace(R.id.notes_titles, NotesDescriptionFragment.newInstance(note)).addToBackStack(null).commit();
-
-//            Intent intent = new Intent(this, NoteDescriptionActivity.class);
-//            intent.putExtra(NoteDescriptionActivity.ARG_NOTE, note);
-//            startActivity(intent);
         }
     }
 }
--- app/src/main/java/com/umbrella/mynotes/NotesFragment.java
+++ app/src/main/java/com/umbrella/mynotes/NotesFragment.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -48,7 +50,6 @@ public class NotesFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         LinearLayout noteList = view.findViewById(R.id.note_container);
         ArrayList<Note> notes = NotesRepository.getNotes();
         for (Note note : notes) {
